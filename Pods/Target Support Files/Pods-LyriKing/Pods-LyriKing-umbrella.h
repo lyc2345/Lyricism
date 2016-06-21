980e3957f4ecb0a32610041859a6e49a43b4e910
Stan Liu <lyc2345@gmail.com>
2016-06-21 17:11:28 +0800

    change project name and folder and identifier, regenerate pod file

---
+++ Pods/Target Support Files/Pods-LyriKing/Pods-LyriKing-umbrella.h
@@ -0,0 +1,6 @@
+#import <Cocoa/Cocoa.h>
+
+
+FOUNDATION_EXPORT double Pods_LyriKingVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LyriKingVersionString[];
+
--- Pods/Target Support Files/Pods-macOS/Pods-macOS-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-
-FOUNDATION_EXPORT double Pods_macOSVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_macOSVersionString[];
-
